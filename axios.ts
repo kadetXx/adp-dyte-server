3fd494609777f1a2d0c2e6925e395ba67a5e24ea
kadetXx <techkadet@gmail.com>
2023-02-06 18:45:56 +0100

    chore: implement error handling on server

--- axios.ts
+++ axios.ts
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const access = btoa(`${process.env.DYTE_ORG_ID}:${process.env.DYTE_API_KEY}`);
+const keys = `${process.env.DYTE_ORG_ID}:${process.env.DYTE_API_KEY}`;
+const access = Buffer.from(keys).toString("base64");
 
 const instance = axios.create({
   baseURL: process.env.DYTE_BASE_URL,
--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import axios from "./axios";
-
 import { auth } from "express-oauth2-jwt-bearer";
 
 dotenv.config();
@@ -16,48 +15,46 @@ const authMiddleware = auth({
 });
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(`<p style="font-family: 'Segoe UI', sans-serif">Running...</p>`);
-});
-
-app.post("/meeting/create", authMiddleware, async (req, res) => {
-  /**
-   * Create dyte meeting here
-   * and auto add user as host
-   */
+app.post("/meeting/create", authMiddleware, async (req, res, next) => {
   try {
     const response = await axios.post("/meetings", {
-      title: "First Meeting",
+      title: req.body.title,
       preferred_region: "ap-south-1",
     });
 
-    console.log(response.data, "response>>>>>");
     res.json(response.data);
   } catch (error) {
-    console.log(error, 'error>>>>>');
+    next(error);
   }
 });
 
-app.post("/meeting/join", authMiddleware, (req, res) => {
-  /**
-   * Join dyte meetings here as a
-   * participant only
-   */
-});
+app.post("/meeting/join", authMiddleware, async (req, res, next) => {
+  try {
+    const params = req.body;
+    const endpoint = `/meetings/${params.meetingId}/participants`;
 
-app.get("/meetings", authMiddleware, (req, res) => {
-  /**
-   * Fetch all meetings the currently
-   * authenticated user has joined and is
-   * still ongoing.
-   */
+    const response = await axios.post(endpoint, {
+      name: params.name,
+      custom_participant_id: params.custom_participant_id,
+      preset_name: params.preset_name,
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use((err: any, req: any, res: any, next: any) => {
-  console.log(err, "caughted>>>>>");
-  res.status(err.statusCode).json({
-    error: err,
+  const statusCode = err.statusCode ?? 500;
+
+  res.status(statusCode).json({
+    success: false,
+    status: statusCode,
+    message: err.message ?? "Unknown Error",
+    stack: process.env.NODE_ENV === "development" ? err.stack : {},
   });
 });
 
